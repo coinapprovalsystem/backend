58ab0332e1003f34585308eaf1becf74730f4b66
chriszhao1994 <574182446@qq.com>
2019-02-02 22:39:53 +0800

    去除com信息

--- me-center-api/src/main/java/com/me/spring/web/handler/MorphedImportBeanDefinitionRegistrar.java
+++ me-center-api/src/main/java/com/me/spring/web/handler/MorphedImportBeanDefinitionRegistrar.java
@@ -1,11 +1,3 @@
-/*
- * Copyright 2018 tuhu.cn All right reserved. This software is the
- * confidential and proprietary information of tuhu.cn ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Tuhu.cn
- */
-
 package com.me.spring.web.handler;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
